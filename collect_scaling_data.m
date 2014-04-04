766bcc3f52a58cbbb06c17a899e05a21857629ed
Ben Cipollini <bcipolli@ucsd.edu>
2014-04-04 09:58:04 -0700

    Write script to gather, and analyze, only the relevant datasets for this experiment.

---
+++ collect_scaling_data.m
@@ -0,0 +1,11 @@
+addpath(genpath(fullfile(fileparts(which(mfilename)), 'data')));
+datasets = { ...
+    'aboitiz_etal_1992', ...  % human data comparison
+    'lamantia_rakic_1990a', ... % age correction
+    'lamantia_rakic_1990b', ... % age correction
+    'rilling_insel_1999a', ... % GI
+    'rilling_insel_1999b', ... %callosal area
+    'tower_1954', ... % grey matter neuron density
+    'wang_etal_2008' % callosal axon density
+};
+collect_all(datasets)
